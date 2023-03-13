673fe64714e73b6e512fe48722756b7100d6bb2b
Raymond <robertkejn@gmail,com>
2023-03-13 21:13:52 +0400

    Initial Commit

---
+++ ht_3_5.2/ht_3_5.2.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+template<class T>
+class Table {
+private:
+	int rows = 0;
+	int cols = 0;
+	T** table;
+public:
+	Table(int rows, int cols) {
+		table = new T * [rows];
+		for (int i = 0; i < rows; ++i) {
+			table[i] = new T[cols];
+		}
+		this->rows = rows;
+		this->cols = cols;
+	}
+	~Table() {
+		for (int i = 0; i < rows; ++i) {
+			delete[] table[i];
+		}
+		delete[] table;
+	}
+
+	T* operator[](int i) const {
+		std::cout << "const\n";
+		return table[i];
+	}
+
+	T* operator[](int i) {
+		std::cout << "not const\n";
+		return table[i];
+	}
+
+
+
+};
+
+int main()
+{
+	auto table = Table<int>(2, 3);
+	table[0][0] = 4;
+	//std::cout << table[0][0];
+}
