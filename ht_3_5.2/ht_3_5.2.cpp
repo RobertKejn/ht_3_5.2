8e743fc7da0910035732dc70bdacf81a1845ab31
Raymond <robertkejn@gmail,com>
2023-03-15 00:45:21 +0400

    Second commit

--- ht_3_5.2/ht_3_5.2.cpp
+++ ht_3_5.2/ht_3_5.2.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <exception>
 
 template<class T>
 class Table {
@@ -8,6 +9,7 @@ private:
 	T** table;
 public:
 	Table(int rows, int cols) {
+		if (rows < 1 || cols < 1) throw std::invalid_argument("Размерности не должны быть меньше 0");
 		table = new T * [rows];
 		for (int i = 0; i < rows; ++i) {
 			table[i] = new T[cols];
@@ -23,22 +25,51 @@ public:
 	}
 
 	T* operator[](int i) const {
-		std::cout << "const\n";
+		if (i < 0 || i >= rows) throw std::out_of_range("Индекс вне заданной размерности");
 		return table[i];
 	}
 
 	T* operator[](int i) {
-		std::cout << "not const\n";
+		if (i < 0 || i >= rows) throw std::out_of_range("Индекс вне заданной размерности");
 		return table[i];
 	}
+	int Size() { return rows; }
 
+	Table<T>& operator=(const Table<T>& t) {
+		for (int i = 0; i < rows; ++i) {
+			delete[] table[i];
+		}
+		delete[] table;
 
+		rows = t.rows;
+		cols = t.cols;
 
+		table = new T* [rows];
+		for (int i = 0; i < rows; ++i) {
+			table[i] = new T[cols];
+		}
+
+		for (int i = 0; i < rows; ++i) {
+			for (int j = 0; j < cols; ++j) {
+				table[i][j] = t.table[i][j];
+			}
+		}
+		return *this;
+	}
 };
 
 int main()
 {
-	auto table = Table<int>(2, 3);
-	table[0][0] = 4;
-	//std::cout << table[0][0];
+	try {
+		auto table1 = Table<int>(3, 3);
+		table1[0][0] = 4;
+		std::cout << table1[0][0]<<'\n';
+		auto table2 = Table<int>(2, 3);
+		table2[0][0] = 10;
+		table1 = table2;
+		std::cout << table1[0][0];
+	}
+	catch (const std::exception& ex) {
+		std::cout << ex.what();
+	}
 }
